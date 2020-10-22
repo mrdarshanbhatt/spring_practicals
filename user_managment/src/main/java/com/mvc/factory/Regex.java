a22e818d246501ba2ff676a5cec0df25910ef451
Mr. Darshan Bhatt <54471535+mrbhatt-88@users.noreply.github.com>
2020-10-22 22:24:47 +0530

    remove redundancy

--- user_managment/src/main/java/com/mvc/factory/Regex.java
+++
@@ -1,8 +0,0 @@
-package com.mvc.factory;
-
-public class Regex {
-
-	public final static String email = "[A-Za-z0-9._%-+]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,4}";
-	public final static String uid = "[0-9]{4}";
-	public final static String mobile = "[0-9]{10}";
-}
